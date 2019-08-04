a7498393a83cf2cefd782cfca6a0b6306d9644f3
mhlee <mkeasy@gmail.com>
2019-08-04 22:56:42 +0900

    폴더/index.js 구조로 통일

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -21,12 +21,14 @@ import Counter from 'components/NewCounter';
 import GlobalStyle from '../../global-styles';
 
 const AppWrapper = styled.div`
+  /*
   max-width: calc(768px + 16px * 2);
   margin: 0 auto;
   display: flex;
   min-height: 100%;
   padding: 0 16px;
   flex-direction: column;
+  */
 `;
 
 export default function App() {
