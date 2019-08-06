b8eb71650d762aedda6eaabc5cd5df61c5fb2870
mhlee <mkeasy@gmail.com>
2019-08-06 16:38:18 +0900

    .

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import './index.sass';
 
 // 도너츠 버튼을 누르면 전체메뉴 레이어 팝업이 생성됩니다.
 class Logo extends Component {
   render() {
-    return <div>[도너츠버튼] - 피그플랜 로고</div>;
+    return (
+      <div>
+        <button className="btn btn-default" type="button">
+          <span
+            className="glyphicon glyphicon-menu-hamburger"
+            aria-hidden="true"
+          />
+        </button>
+        피그플랜 로고
+      </div>
+    );
   }
 }
 
