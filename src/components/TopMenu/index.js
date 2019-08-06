9c18e95546c31b3e04b1a21a6762455c56c337e3
mhlee <mkeasy@gmail.com>
2019-08-06 13:10:25 +0900

    support sass & tachyons , bootstrap

--- src/components/TopMenu/index.js
+++ src/components/TopMenu/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { Button, Fade } from 'reactstrap';
+import { Nav, NavItem, NavLink, Button, Fade, Hide } from 'reactstrap';
 
 import { observable, action } from 'mobx';
 import { observer } from 'mobx-react';
 
+import './index.scss';
+
 @observer
 class TopMenu extends Component {
   @observable
-  fadeIn0 = false;
+  fadeIn0 = true;
 
   @observable
   fadeIn1 = false;
@@ -39,59 +41,104 @@ class TopMenu extends Component {
   render() {
     return (
       <div>
-        <nav>
-          <ul>
-            <li>
-              <a href="#" onClick={this.toggle0}>
-                일보
-              </a>
-              <Fade in={this.fadeIn0} className="mb-3">
-                <Button>사육현황</Button>
-                <Button>교육현황</Button>
-                <Button>임신사고현황</Button>
-                <Button>생산현황</Button>
-                <Button>전입 및 전출/폐사현황</Button>
-                <Button>거래기록</Button>
-              </Fade>
-            </li>
+        <Nav className="bg-yellow">
+          <NavItem>
+            <NavLink className="fl" onClick={this.toggle0}>
+              일보
+            </NavLink>
+            <NavLink className="fl" onClick={this.toggle1}>
+              IoT
+            </NavLink>
+            <NavLink className="fl">성적그래프</NavLink>
+            <NavLink className="fl" onClick={this.toggle2}>
+              양돈정보
+            </NavLink>
+            <NavLink className="fn">퀵메뉴</NavLink>
+          </NavItem>
+        </Nav>
 
-            <li>
-              <a href="#" onClick={this.toggle1}>
-                IoT
-              </a>
-              <Fade in={this.fadeIn1} className="mb-3">
-                <Button>모돈자동급이기</Button>
-                <Button>군사급이기</Button>
-                <Button>환경센서</Button>
-                <Button>음수관리기</Button>
-                <Button>사료빈관리기</Button>
-                <Button>돈선별기</Button>
-                <Button>자돈급이기</Button>
-                <Button>냉방기</Button>
-                <Button>전기화재</Button>
-              </Fade>
-            </li>
+        <Nav className="bg-white">
+          <NavItem>
+            <Fade in={this.fadeIn0} className="mb-3">
+              {this.fadeIn0 && (
+                <>
+                  <Button outline color="primary" className="ma1 fl">
+                    사육현황
+                  </Button>
+                  <Button outline color="primary" className="ma1 fl">
+                    교육현황
+                  </Button>
+                  <Button outline color="primary" className="ma1 fl">
+                    임신사고현황
+                  </Button>
+                  <Button outline color="primary" className="ma1 fl">
+                    생산현황
+                  </Button>
+                  <Button outline color="primary" className="ma1 fl">
+                    전입 및 전출/폐사현황
+                  </Button>
+                  <Button outline color="primary" className="ma1 fn">
+                    거래기록
+                  </Button>
+                </>
+              )}
+            </Fade>
+          </NavItem>
 
-            <li>
-              <a href="#">성적그래프</a>
-            </li>
+          <NavItem>
+            <Fade in={this.fadeIn1} className="mb-3">
+              {this.fadeIn1 && (
+                <>
+                  <Button outline color="primary" className="ma1 fl">
+                    모돈자동급이기
+                  </Button>
+                  <Button outline color="primary" className="ma1 fl">
+                    군사급이기
+                  </Button>
+                  <Button outline color="primary" className="ma1 fl">
+                    환경센서
+                  </Button>
+                  <Button outline color="primary" className="ma1 fl">
+                    음수관리기
+                  </Button>
+                  <Button outline color="primary" className="ma1 fl">
+                    사료빈관리기
+                  </Button>
+                  <Button outline color="primary" className="ma1 fl">
+                    돈선별기
+                  </Button>
+                  <Button outline color="primary" className="ma1 fl">
+                    자돈급이기
+                  </Button>
+                  <Button outline color="primary" className="ma1 fl">
+                    냉방기
+                  </Button>
+                  <Button outline color="primary" className="ma1 fn">
+                    전기화재
+                  </Button>
+                </>
+              )}
+            </Fade>
+          </NavItem>
 
-            <li>
-              <a href="#" onClick={this.toggle2}>
-                양돈정보
-              </a>
-              <Fade in={this.fadeIn2} className="mb-3">
-                <Button>도매시세</Button>
-                <Button>양돈기사</Button>
-                <Button>날씨</Button>
-              </Fade>
-            </li>
-
-            <li>
-              <a href="#">퀵메뉴</a>
-            </li>
-          </ul>
-        </nav>
+          <NavItem>
+            <Fade in={this.fadeIn2} className="mb-3">
+              {this.fadeIn2 && (
+                <>
+                  <Button outline color="primary" className="ma1 fl">
+                    도매시세
+                  </Button>
+                  <Button outline color="primary" className="ma1 fl">
+                    양돈기사
+                  </Button>
+                  <Button outline color="primary" className="ma1 fn">
+                    날씨
+                  </Button>
+                </>
+              )}
+            </Fade>
+          </NavItem>
+        </Nav>
       </div>
     );
   }
