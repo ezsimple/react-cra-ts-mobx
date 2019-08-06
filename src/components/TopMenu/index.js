9ac0af13e8d637449420d4690dc94f832e5bde53
mhlee <mkeasy@gmail.com>
2019-08-06 09:59:28 +0900

    .

--- src/components/TopMenu/index.js
+++ src/components/TopMenu/index.js
@@ -39,45 +39,59 @@ class TopMenu extends Component {
   render() {
     return (
       <div>
-        <Button color="primary" onClick={this.toggle0}>
-          일보
-        </Button>
-        <Fade in={this.fadeIn0} className="mb-3">
-          <Button>사육현황</Button>
-          <Button>교육현황</Button>
-          <Button>임신사고현황</Button>
-          <Button>생산현황</Button>
-          <Button>전입 및 전출/폐사현황</Button>
-          <Button>거래기록</Button>
-        </Fade>
+        <nav>
+          <ul>
+            <li>
+              <a href="#" onClick={this.toggle0}>
+                일보
+              </a>
+              <Fade in={this.fadeIn0} className="mb-3">
+                <Button>사육현황</Button>
+                <Button>교육현황</Button>
+                <Button>임신사고현황</Button>
+                <Button>생산현황</Button>
+                <Button>전입 및 전출/폐사현황</Button>
+                <Button>거래기록</Button>
+              </Fade>
+            </li>
 
-        <Button color="primary" onClick={this.toggle1}>
-          IoT
-        </Button>
-        <Fade in={this.fadeIn1} className="mb-3">
-          <Button>모돈자동급이기</Button>
-          <Button>군사급이기</Button>
-          <Button>환경센서</Button>
-          <Button>음수관리기</Button>
-          <Button>사료빈관리기</Button>
-          <Button>돈선별기</Button>
-          <Button>자돈급이기</Button>
-          <Button>냉방기</Button>
-          <Button>전기화재</Button>
-        </Fade>
+            <li>
+              <a href="#" onClick={this.toggle1}>
+                IoT
+              </a>
+              <Fade in={this.fadeIn1} className="mb-3">
+                <Button>모돈자동급이기</Button>
+                <Button>군사급이기</Button>
+                <Button>환경센서</Button>
+                <Button>음수관리기</Button>
+                <Button>사료빈관리기</Button>
+                <Button>돈선별기</Button>
+                <Button>자돈급이기</Button>
+                <Button>냉방기</Button>
+                <Button>전기화재</Button>
+              </Fade>
+            </li>
 
-        <Button color="primary">성적그래프</Button>
+            <li>
+              <a href="#">성적그래프</a>
+            </li>
 
-        <Button color="primary" onClick={this.toggle2}>
-          양돈정보
-        </Button>
-        <Fade in={this.fadeIn2} className="mb-3">
-          <Button>도매시세</Button>
-          <Button>양돈기사</Button>
-          <Button>날씨</Button>
-        </Fade>
+            <li>
+              <a href="#" onClick={this.toggle2}>
+                양돈정보
+              </a>
+              <Fade in={this.fadeIn2} className="mb-3">
+                <Button>도매시세</Button>
+                <Button>양돈기사</Button>
+                <Button>날씨</Button>
+              </Fade>
+            </li>
 
-        <Button color="primary">퀵메뉴</Button>
+            <li>
+              <a href="#">퀵메뉴</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
